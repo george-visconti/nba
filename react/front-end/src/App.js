4e7135035c88d358aac3867d45acd0392a8d1258
George Visconti Jr <georgevisconti00@gmail.com>
2021-08-11 21:52:49 -0400

    created a react app for the front end

---
+++ react/front-end/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This application will display the NBA stat leaders from the 2020-2021 season. 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
