ba6c3f11684fd8c5277bd997023cade7c73b8373
joe_diprofio <joseph.diprofio18@stjohns.edu>
2021-08-17 12:52:27 -0400

    front end bug fixed

--- react/front-end/src/AppElements.js
+++ react/front-end/src/AppElements.js
@@ -9,16 +9,19 @@ export const CardContainer= styled.div`
     height 300px;
     width: 200px;
     align-items:center;
-    margin-bottom:20px;
     background-image: url(${court});
     background-position: center;
+    @media (max-width: 1024px){
+        margin-bottom:15px;
+    }
 `
 
 export const Page= styled.div`
         display: flex;
         flex-direction:column;
-        align-items: center;
-        justify-content:center;
+        align-items:center;
+        justify-content: center;
+        min-height:100vh;
         width:100%;
         background: #479bf5;
 `
@@ -28,9 +31,7 @@ export const CardRow= styled.div`
     flex-direction:row;
     justify-content: space-between;
     align-items:center;
-    min-height:100vh;
     width:100%;
-    margin-top: -50px;
     @media (max-width: 1024px){
         flex-direction:column;
         margin-top:40px;
@@ -53,12 +54,11 @@ export const Picture= styled.img`
 
 export const HeaderText= styled.p`
     font-size: 30px;
-    align-text:center;
     text-decoration:underline;
     text-decoration-thickness: 2px;
     font-family: 'Open Sans', sans-serif;
-    margin-top: 60px;
     font-weight: bold;
+    margin-bottom: 25px;
     @media (max-width: 600px){
         font-size: 18px;
         
